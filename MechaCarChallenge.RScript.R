41e735fc55aaa5ceef20535a7dc4cf8ce9b74ebf
Jesse Kelsey <jessekelsey@Jesses-MacBook.local>
2021-11-22 20:51:58 -0800

    Adding RScript

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -10,3 +10,10 @@ Suspension_Coil_Table <-read.csv(file='Suspension_Coil.csv', check.names = F, st
 head(Suspension_Coil_Table)
 total_summary <-Suspension_Coil_Table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
 lot_summary <-Suspension_Coil_Table%>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+t.test(Suspension_Coil_Table$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil_Table, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(Suspension_Coil_Table, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(Suspension_Coil_Table, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
